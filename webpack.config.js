f42456a372aaa08ff9f568fe61b136290398c8a8
Ian <ianchen0419@gmail.com>
2019-12-11 11:35:37 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -14,26 +14,26 @@ module.exports = {
                 test: /\.(scss|sass)$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
+                    'css-loader',
+                    'sass-loader'
                 ]
             },
             {
                 test: /\.css$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "css-loader"
+                    'css-loader'
                 ]
             },
             {
                 test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
-                loader: "url-loader"
+                loader: 'url-loader'
             }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "bundle.css"
+            filename: 'bundle.css'
         }),
         new webpack.ProvidePlugin({
             $: 'jquery',
